0e2866199216a98e5685d1faa2e1013ea2cd2102
Jordan Zhang <chrn.zhng@gmail.com>
2017-06-13 12:31:15 -0600

    Finished

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -9,10 +9,15 @@ export default class SizeChanger extends Component {
   }
 
   // componentWillReceiveProps
+    componentWillReceiveProps(newProps){
+    this.setState({
+      allowEdit: newProps.allowEdit
+    })
+  }
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" onChange={ (e) => this.props.update(e.target.value) } disabled={ this.state.allowEdit === "false" }>
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
